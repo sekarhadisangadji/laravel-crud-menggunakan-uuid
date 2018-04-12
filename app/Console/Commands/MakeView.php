c8cb6f5ca44bdf15bf6b661e63adf32f4d3f2b88
hadisangadji <sekarhadisangadjii@gmail.com>
2018-04-12 18:38:56 +0700

    update

---
+++ app/Console/Commands/MakeView.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use File;
+
+class MakeView extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:view {view}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Membuat blade template.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $view = $this->argument('view');
+
+        $path = $this->viewPath($view);
+
+        $this->createDir($path);
+
+        if (File::exists($path))
+        {
+            $this->error("File {$path} sudah ada!");
+            return;
+        }
+
+        File::put($path, $path);
+
+        $this->info("File {$path} berhasil dibuat.");
+    }
+
+     /**
+     * Get the view full path.
+     *
+     * @param string $view
+     *
+     * @return string
+     */
+    public function viewPath($view)
+    {
+        $view = str_replace('.', '/', $view) . '.blade.php';
+
+        $path = "resources/views/{$view}";
+
+        return $path;
+    }
+
+    /**
+     * Create view directory if not exists.
+     *
+     * @param $path
+     */
+    public function createDir($path)
+    {
+        $dir = dirname($path);
+
+        if (!file_exists($dir))
+        {
+            mkdir($dir, 0777, true);
+        }
+    }
+}
